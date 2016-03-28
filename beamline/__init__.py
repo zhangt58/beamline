6f0d4a6bb67fc32b5db5539ca4aa13c6a4a5d722
Tong Zhang <warriorlance@gmail.com>
2016-03-28 15:19:00 +0800

    add modules

--- beamline/__init__.py
+++ beamline/__init__.py
@@ -3,30 +3,42 @@ from .pltutils   import plotLattice, makeBeamline
 from .blparser   import madParser
 from .lattice    import Lattice, LteParser
 from .simulation import Simulator
-from .datautils  import DataExtracter
+from .datautils  import DataExtracter, DataVisualizer, DataStorage
+from .element    import MagBlock
+from .element    import ElementCharge
+from .element    import ElementCsrcsben, ElementQuad
+from .element    import ElementCsrdrift, ElementDrift, ElementLscdrift
+from .element    import ElementKicker
+from .element    import ElementMark, ElementWatch, ElementMoni
+from .element    import ElementRfcw, ElementRfdf, ElementWake
+from .element    import ElementBeamline
+from .models     import Models
+
 
 __version__ = "1.1.3"
 __author__ = "Tong Zhang"
 
 __doc__ = """
-    lattice operation module
+    lattice operation/online module
 
     Version: %s
     Author: Tong Zhang (zhangtong@sinap.ac.cn)
 
-    This python package is created for lattice objectification, 
-    manipulation and visualization.
+    This python package is created for lattice online modeling, 
+    manipulating and visualising.
 """ % (__version__)
 
-def testf(arg1, arg2='arg2'):
-    """test function
+allElements = [ElementCharge,   ElementCsrcsben, ElementQuad, 
+               ElementCsrdrift, ElementDrift,    ElementLscdrift,
+               ElementKicker,   ElementMark,     ElementWatch, 
+               ElementMoni,     ElementRfcw,     ElementRfdf, 
+               ElementWake,     ElementBeamline]
 
-    test test test
-    """
-    return arg1 + arg2
-
-__all__ = [Drift, Rbend, Undulator, Quadrupole, 
+__all__ = [
+           Drift, Rbend, Undulator, Quadrupole, 
            plotLattice, makeBeamline, madParser, 
            Lattice, LteParser,
            Simulator,
-           DataExtracter]
+           DataExtracter, DataVisualizer, DataStorage,
+           Models,
+           ].extend(allElements)
