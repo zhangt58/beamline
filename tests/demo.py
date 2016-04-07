5de23214f9352588ece29b835128521e32a8b51f
Tong Zhang <warriorlance@gmail.com>
2016-04-07 21:35:36 +0800

    add demo for lattice visualization

--- tests/demo.py
+++ tests/demo.py
@@ -1,7 +1,9 @@
-
+#!/usr/bin/python
 # coding: utf-8
 
-# ## Demonstration for using *beamline* python module to do online modeling.
+# ## Code demonstration for using *beamline* python package to do online modeling
+# 
+#                                         Tong Zhang, March, 2016 (draft)
 #     
 # For example, define lattice configuration for a 4-dipole chicane with quads:
 #                     
@@ -14,17 +16,30 @@
 #             + dipole + drift + dipole
 #             + drift  + quad  + drift
 # 
-# Below is the typical workflow:
+# Below is the typical workflow and interleaved comments.
 
-# In[1]:
+# In[169]:
 
 import beamline
 import os
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import copy
 
 
-# #### STEP 1: define common information, two different input parameter formats
+# ### Section 1: Magnetic Elements Modeling
 
-# In[2]:
+# The very first step need to push forward is to correctly model the physical elements (one by one), in the *beamline* package, magnet components classes could be found in *element* module, e.g. quadrupole is abstracted in *ElementQuad* class, charge is in *ElementCharge*, etc., they are all inherited from *MagBlock*.
+# 
+# The common or shared information/configuration for all these elements could be predefined in *MagBlock* class, e.g. we can put information like facility name, time stamp, author, etc., common information is presumed not changed, so please defined in the first step (see STEP 1).
+# 
+# To set the elements' configuration, method *setConf(config, type)* could be used to, in which 'config' is either configuration string with the format like "k1=10.0,l=0.1" or python dictionary like "{'k1': 10.0, 'l': 0.1}", and 'type' is the configuration type to be confiugred, could be 'comm' [common configuration], 'ctrl' [control configuration], 'simu' [simulation configuration], 'misc' [miscellaneous configuration] and 'all' [all configurations].
+# 
+# The unit between EPICS PV values and real physical variables usually are required to do conversions, so in the design stage, the method *unitTrans(inval,direction = '+',transfun = None)* is created for handling this issue. One can define this conversion function at the class stage, but this approach is limited to the case that all the elements with the same type only could share the same conversion function, which is not proper in the real situation. Thus, *transfun* is created as the input function parameter for *unitTrans* method, which is a user-defined function for each element object.
+
+# #### STEP 1: define common information
+
+# In[170]:
 
 #commdinfo = {'DATE': '2016-03-22', 'AUTHOR': 'Tong Zhang'}
 comminfo = 'DATE = 2016-03-24, AUTHOR = Tong Zhang'
@@ -33,13 +48,15 @@ beamline.MagBlock.setCommInfo(comminfo)
 
 # #### STEP 2: create elements
 
-# In[22]:
+# In[173]:
 
-# charge
+# charge, this is visual element for the real accelerator, but is a must for elegant tracking
 chconf = {'total':1e-9}
 q = beamline.ElementCharge(name = 'q', config = chconf)
 
-# csrcsben
+# csrcsben, use elegant element name
+# simconf is complementary configurations for elegant tracking, 
+# should set with setConf(simconf, type='simu') method
 simconf = {"edge1_effects": 1,
            "edge2_effects":1,
            "hgap":0.015,
@@ -55,7 +72,6 @@ angle = 0.1 # rad
 
 B1 = beamline.ElementCsrcsben(name = 'b1', config = {'angle':angle, 'e1':0, 'e2':angle})
 B1.setConf(simconf, type = 'simu')
-    
 B2 = beamline.ElementCsrcsben(name = 'b2', config = {'angle':-angle, 'e1':-angle, 'e2':0})
 B3 = beamline.ElementCsrcsben(name = 'b3', config = {'angle':-angle, 'e1':0,      'e2':-angle})
 B4 = beamline.ElementCsrcsben(name = 'b4', config = {'angle': angle, 'e1':angle,  'e2':0})
@@ -66,21 +82,36 @@ B4.setConf(simconf, type = 'simu')
 # drift
 D0 = beamline.ElementDrift(name = 'D0', config = "l=1.0")
 
+
+# In[174]:
+
 # quad
-Q1 = beamline.ElementQuad(name = 'Q1', config = "k1 = 10, l = 0.1")
+# user-defined unit conversion function, 
+# direction '+' means convertion from EPICS PV value to physical value,
+# direction '-' means convertion from physical value to EPICS PV value,
+def fUnitTrans(val, direction):
+    if direction == '+':
+        return val*4.0
+    else:
+        return val*0.25
+
+# create instance and apply user-defined unit conversion function
+Q1 = beamline.ElementQuad(name = 'Q1', config = "k1 = 10, l = 0.5")
 simuconf = {'tilt':"pi 4 /"}
 Q1.setConf(simuconf, type = 'simu')
 # control configurations for Q1
 ctrlconf = {"k1":{'pv':"sxfel:lattice:Q09",'val':''}}
 Q1.setConf(ctrlconf, type = 'ctrl')
-print Q1.dumpConfig(type='online')
+Q1.transfun = fUnitTrans # apply unit conversion function
+# print 'online' configuration, 'online' will replace simulation field with control field 
+print Q1.dumpConfig(type='online') 
 #Q1.printConfig(type = 'simu')
 Q1.printConfig(type = 'all')
 
 
 # #### STEP 3: make lattice beamline
 
-# In[4]:
+# In[175]:
 
 # METHOD 1: CANNOT get all configurations
 # use 'ElementBeamline' class of 'element' module
@@ -96,62 +127,67 @@ bl = beamline.ElementBeamline(name = 'bl', config = {'lattice':latstr})
 
 # METHOD 2: CAN get all configurations
 # use 'Models' class of 'models' module
-latline_online = beamline.Models(name = 'blchi', mode = 'online')
+# change mode to be 'simu' to start simulation mode, 
+# 'online' mode will trig EPICS get/put processes when control configurations 
+# could be found in elements' configuration. 
+latline_online = beamline.Models(name = 'blchi', mode = 'online') 
 qline = (D0, Q1, D0)
 chi   = (B1, D0, B2, D0, D0, B3, D0, B4)
 latline_online.addElement(q, qline, chi, qline)
-#latline_online.getCtrlConf()
     
 # show defined elements number
 #print beamline.MagBlock.sumObjNum()
 
 
-# In[ ]:
+# In[181]:
 
-
-
-
-# In[5]:
-
-latline_online.getAllConfig(fmt='dict')['Q1']
-
-
-# In[6]:
-
-qcharge = filter(lambda x:x.name == 'q', latline_online._lattice_eleobjlist)[0]
-qcharge.setConf('total = 1e-9',type='simu')
-
-
-# In[7]:
-
+# get 'b1' element from created model
 eleb1 = latline_online.getElementsByName('b1')
+print eleb1.name
+# change b1 configuration, e.g. angle
+eleb1.setConf('angle=0.5', type = 'simu')
+eleb1.printConfig()
+
+# print out all added elements
 latline_online.printAllElements()
 
-
-# In[8]:
-
-eleb1.setConf('angle = 0.1',type='simu')
+# get configuration of 'Q1'
+print latline_online.getAllConfig(fmt='dict')['Q1']
 
 
-# In[9]:
+# In[182]:
+
+eleb1.printConfig()
+
+
+# In[183]:
 
 eleQ1 = latline_online.getElementsByName('Q1')
 eleQ1.printConfig(type='all')
 
 
-# In[10]:
+# In[184]:
 
-latline_online.putCtrlConf(eleQ1, 'k1', 1)
+# update Q1's EPICS PV value
+latline_online.putCtrlConf(eleQ1, 'k1', 2.5, type = 'real')
+eleQ1.printConfig(type='all')
 
 
-# In[11]:
+# In[185]:
 
 latline_online.getAllConfig(fmt='dict')
 
 
+# ### Section 2: Lattice modeling
+
 # #### STEP 4: create Lattice instance, make simulation required input files
 
-# In[12]:
+# In[186]:
+
+eleb1.setConf('angle=0.1', type = 'simu')
+
+
+# In[187]:
 
 # e.g. '.lte' for elegant tracking, require all configurations
 latins = beamline.Lattice(latline_online.getAllConfig())
@@ -159,14 +195,14 @@ latfile = os.path.join(os.getcwd(), 'tracking/test.lte')
 latins.generateLatticeFile(latline_online.name, latfile)
 
 
-# In[13]:
+# In[189]:
 
 latins.dumpAllElements()
 
 
 # #### STEP 5: simulation with generated lattice file
 
-# In[14]:
+# In[190]:
 
 simpath = os.path.join(os.getcwd(), 'tracking')
 elefile = os.path.join(simpath, 'test.ele')
@@ -179,12 +215,12 @@ elesim.setPath(simpath)
 elesim.setInputfiles(ltefile = latfile, elefile = elefile)
 
 
-# In[15]:
+# In[191]:
 
 elesim.doSimulation()
 
 
-# In[16]:
+# In[192]:
 
 # data columns could be extracted from simulation output files, to memory or h5 files.
 data_tp    = elesim.getOutput(file = 'test.out', data = ('t', 'p'  ))#, dump = h5out)
@@ -194,34 +230,97 @@ data_setax = elesim.getOutput(file = 'test.twi', data = ('s', 'etax'))
 
 # #### visualize data
 
-# In[17]:
+# In[193]:
 
-import matplotlib.pyplot as plt
-get_ipython().magic(u'matplotlib inline')
-
-
-# In[18]:
-
-plt.plot(data_tp[:,0],data_tp[:,1],'.')
-plt.xlabel('$t\,[s]$')
-plt.ylabel('$\gamma$')
+# %matplotlib inline
 
 
-# In[19]:
+# In[194]:
 
-plt.plot(data_sSx[:,0],data_sSx[:,1],'-')
-plt.ylabel('$\sigma_x\,[\mu m]$')
-plt.xlabel('$s\,[m]$')
-
-
-# In[20]:
-
-plt.plot(data_setax[:,0],data_setax[:,1],'-')
-plt.ylabel('$\eta_{x}\,[m]$')
-plt.xlabel('$s\,[m]$')
+fig = plt.figure(1)
+ax1 = fig.add_subplot(221)
+ax1.plot(data_tp[:,0],data_tp[:,1],'.')
+ax1.set_xlabel('$t\,[s]$')
+ax1.set_ylabel('$\gamma$')
 
 
-# In[ ]:
+# In[195]:
+
+ax2 = fig.add_subplot(222)
+ax2.plot(data_sSx[:,0],data_sSx[:,1],'-')
+ax2.set_ylabel('$\sigma_x\,[\mu m]$')
+ax2.set_xlabel('$s\,[m]$')
 
 
+# In[196]:
 
+ax3 = fig.add_subplot(223)
+ax3.plot(data_setax[:,0],data_setax[:,1],'r-', lw=3,)
+ax3.set_ylabel('$\eta_{x}\,[m]$')
+ax3.set_xlabel('$s\,[m]$')
+
+
+# In[153]:
+
+"""
+# Scan parameter: final Dx v.s. angle of B1
+import numpy as np
+dx = []
+thetaArray = np.linspace(0.05,0.3,20)
+for theta in thetaArray:
+    eleb1.setConf({'angle':theta}, type = 'simu')
+    latins = beamline.Lattice(latline_online.getAllConfig())
+    latins.generateLatticeFile(latline_online.name, latfile)
+    elesim.doSimulation()
+    data = elesim.getOutput(file = 'test.twi', data = (['etax']))
+    dx.append(data[-1])
+dxArray = np.array(dx)
+
+
+# In[197]:
+
+plt.plot(thetaArray, dxArray, 'r')
+"""
+
+
+# #### Lattice layout visualization
+
+ptches, xr, yr = latline_online.draw(showfig=False)
+
+def copy_patches(ptches0):
+    copyed_ptches = []
+    for pt in ptches0:
+        pth = pt.get_path().deepcopy()
+        ptch = patches.PathPatch(pth, 
+                        lw=pt.get_lw(), 
+                        fc=pt.get_fc(), 
+                        ec=pt.get_ec(), 
+                        alpha=pt.get_alpha())
+        copyed_ptches.append(ptch)
+
+    return copyed_ptches
+
+newptches = copy_patches(ptches)
+
+#for i,val in enumerate(newptches):
+#    print id(newptches[i]), id(ptches[i])
+
+ax4 = fig.add_subplot(224)
+[ax4.add_patch(i) for i in newptches]
+ax4.set_xlim(xr)
+ax4.set_ylim(yr)
+
+ax3t = ax3.twinx()
+[ax3t.add_patch(i) for i in ptches]
+xr3 = ax3.get_xlim() 
+yr3 = ax3.get_ylim() 
+x0, x1 = min(xr[0],xr3[0]), max(xr[1], xr3[1])
+y0, y1 = min(yr[0],yr3[0]), max(yr[1], yr3[1])
+ax3t.set_xlim(x0, x1)
+ax3t.set_ylim(y0, y1*5)
+ax3.set_xlim(x0, x1)
+ax3.set_ylim(y0, y1)
+
+ax3.grid()
+
+plt.show()
